a2722405579512f89a81025f549d9da5a9a2c158
Sean McIntyre <sean_mcintyre@hotmail.com>
2024-01-02 15:00:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,144 +3,117 @@ import './chat-style.css';
 
 const dssChatApp = document.createElement('dssChatApp')
 dssChatApp.innerHtml = `
-<link rel="stylesheet" link="./chat-style.css" />
-<section>
-  <div class="dss-chat-floatButton">
-    <span class="chat-icon-group button-section">
-      <span
-        name="svgImg1"
-        value="svgImg1"
-        data-component-id="DssChatBotButton"
-        class="
-          semi-transparent-button
-          text-center
-          cursor-pointer
-          shadow
-          fab-icon
-        "
-      >
-        <svg
-          viewBox="0 0 1024 1024"
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-          class="svg-icon"
-          style="
-            width: 4em;
-            height: 4em;
-            vertical-align: middle;
-            fill: currentColor;
-            overflow: hidden;
-            padding-top: 0.7em;
+  <section>
+    <div class="dss-chat-floatButton">
+      <span class="chat-icon-group button-section " @click=${this._openChat} >
+        <span
+        
+          name="svgImg1"
+          value="svgImg1"
+          data-component-id="DssChatBotButton"
+          class="
+            semi-transparent-button
+            text-center
+            cursor-pointer
+            shadow
+            fab-icon
           "
         >
-          <path
-            _ngcontent-woe-c171=""
-            d="M881.777778 369.777778H483.555556c-48.355556 0-85.333333 36.977778-85.333334 85.333333v227.555556c0 45.511111 36.977778 85.333333 85.333334 85.333333h284.444444l113.777778 113.777778v-113.777778c48.355556 0 85.333333-39.822222 85.333333-85.333333v-227.555556c0-48.355556-36.977778-85.333333-85.333333-85.333333z"
-            fill=""
-          ></path>
-          <path
-            _ngcontent-woe-c171=""
-            d="M540.444444 170.666667H142.222222C93.866667 170.666667 56.888889 207.644444 56.888889 256v227.555556c0 45.511111 36.977778 85.333333 85.333333 85.333333v113.777778l113.777778-113.777778h85.333333v-113.777778c0-79.644444 62.577778-142.222222 142.222223-142.222222h142.222222V256c0-48.355556-36.977778-85.333333-85.333334-85.333333z"
-            fill=""
-          ></path>
-        </svg>
-      </span>
-      <h5 class="mb-3">Live Chat</h5>
-      <span
-        name="svgImg1"
-        value="svgImg1"
-        data-component-id="DssChatBotButton"
-        class="
-          semi-transparent-button
-          text-center
-          cursor-pointer
-          shadow
-          fab-icon
-        "
-      >
-        <svg
-          viewBox="0 0 20.517 20.517"
-          id="forum-24px"
-          xmlns="http://www.w3.org/2000/svg"
-          width="32"
-          height="32"
-          class="mt-3"
+          <svg
+            viewBox="0 0 1024 1024"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            class="svg-icon"
+            style="
+              width: 4em;
+              height: 4em;
+              vertical-align: middle;
+              fill: currentColor;
+              overflow: hidden;
+              padding-top: 0.7em;
+            "
+          >
+            <path
+              d="M881.777778 369.777778H483.555556c-48.355556 0-85.333333 36.977778-85.333334 85.333333v227.555556c0 45.511111 36.977778 85.333333 85.333334 85.333333h284.444444l113.777778 113.777778v-113.777778c48.355556 0 85.333333-39.822222 85.333333-85.333333v-227.555556c0-48.355556-36.977778-85.333333-85.333333-85.333333z"
+              fill=""
+            ></path>
+            <path
+              d="M540.444444 170.666667H142.222222C93.866667 170.666667 56.888889 207.644444 56.888889 256v227.555556c0 45.511111 36.977778 85.333333 85.333333 85.333333v113.777778l113.777778-113.777778h85.333333v-113.777778c0-79.644444 62.577778-142.222222 142.222223-142.222222h142.222222V256c0-48.355556-36.977778-85.333333-85.333334-85.333333z"
+              fill=""
+            ></path>
+          </svg>
+        </span>
+        <h5 class="mb-3">Live Chat</h5>
+        <span
+        
+          name="svgImg1"
+          value="svgImg1"
+          data-component-id="DssChatBotButton"
+          class="
+            semi-transparent-button
+            text-center
+            cursor-pointer
+            shadow
+            fab-icon
+          "
         >
-          <path
-            id="Path_587"
-            data-name="Path 587"
-            d="M0,0H20.517V20.517H0Z"
-            fill="none"
-          ></path>
-          <path
-            _ngcontent-woe-c171=""
-            id="Path_588"
-            data-name="Path 588"
-            d="M18.243,5.42h-1.71v7.694H5.42v1.71a.857.857,0,0,0,.855.855h9.4L19.1,19.1V6.274A.857.857,0,0,0,18.243,5.42Zm-3.42,5.129V2.855A.857.857,0,0,0,13.968,2H2.855A.857.857,0,0,0,2,2.855V14.823L5.42,11.4h8.549A.857.857,0,0,0,14.823,10.549Z"
-            transform="translate(-0.29 -0.29)"
-            fill="#fff"
-          ></path>
-        </svg>
+          <svg
+            viewBox="0 0 20.517 20.517"
+            id="forum-24px"
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            class="mt-3"
+          >
+            <path
+              id="Path_587"
+              data-name="Path 587"
+              d="M0,0H20.517V20.517H0Z"
+              fill="none"
+            ></path>
+            <path
+              _ngcontent-woe-c171=""
+              id="Path_588"
+              data-name="Path 588"
+              d="M18.243,5.42h-1.71v7.694H5.42v1.71a.857.857,0,0,0,.855.855h9.4L19.1,19.1V6.274A.857.857,0,0,0,18.243,5.42Zm-3.42,5.129V2.855A.857.857,0,0,0,13.968,2H2.855A.857.857,0,0,0,2,2.855V14.823L5.42,11.4h8.549A.857.857,0,0,0,14.823,10.549Z"
+              transform="translate(-0.29 -0.29)"
+              fill="#fff"
+            ></path>
+          </svg>
+        </span>
+        <h5 class="mb-3">Chat Bot</h5>
+        <div
+          id="modal-container-chat"
+          class="
+            modal-container-chat
+            shadow-lg
+            position-fixed
+            modal-section-chat
+            ${this.isChatEngaged ? '' : 'hidden'}
+          "
+        >
+          <iframe
+            id="chat-iframe"
+            class="w-100 h-100 border"
+            src="http://localhost:8080/app/chat?orgId=globalorg-us&liveChat=true"
+            title="description"
+          ></iframe>
+        </div>
       </span>
-      <h5 class="mb-3">Chat Bot</h5>
-      <div
-        id="modal-container-chat"
-        class="
-          modal-container-chat
-          shadow-lg
-          position-fixed
-          modal-section-chat
-          hidden
-        "
-      >
-        <iframe
-          id="chat-iframe"
-          class="w-100 h-100 border"
-          src="http://localhost:8080/app/chat?orgId=globalorg-us&liveChat=true"
-          title="description"
-        ></iframe>
-      </div>
-    </span>
-  </div>
-</section>
+    </div>
+  </section>
 `
 
 class DssChatApp extends HTMLElement {
-  // static get observedAttributes() {
-  //   return ['theme','icon'];
-  // }
 
   constructor() {
     super();
+    this.isChatEngaged = false;
   }
 
-  // More JS goes here.
-const iframeContainer = document.getElementById('modal-container-chat');
-
-const chatButtonOpen = document.querySelector(
-  '[data-component-id="DssChatBotButton"]'
-);
-
-let showPopup = false;
-
-window.addEventListener('message', (e) => {
-  if (e.origin !== 'http://localhost:8080') return;
-  showPopup = e.data.showPopup;
-
-  if (showPopup) {
-    chatButtonOpen.classList.add('hidden');
-    iframeContainer.classList.remove('hidden');
-  } else {
-    chatButtonOpen.classList.remove('hidden');
-    iframeContainer.classList.add('hidden');
-  }
-});
-
-chatButtonOpen.addEventListener('click', (event) => {
-  chatButtonOpen.classList.add('hidden');
-  iframeContainer.classList.remove('hidden');
-});
-  
+  _openChat() {
+    this.isChatEngaged = !this.isChatEngaged
+  }  
 }
 
 customElements.define('dss-chat-app', DssChatApp)
